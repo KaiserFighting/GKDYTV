8416674b8170ea08123432e01b75226ae4f629b2
kaiserfighting <18192504529@163.com>
2017-06-29 14:37:10 +0800

    添加扩展

--- DouYuTV/DouYuTV/Classes/Home/Controler/HomeViewController.swift
+++ DouYuTV/DouYuTV/Classes/Home/Controler/HomeViewController.swift
@@ -12,24 +12,35 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //1.设置UI界面
+        setupUI()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+//MARK:--设置UI界面
+extension HomeViewController {
+    
+    fileprivate func setupUI(){
+        //1.设置导航栏
+        setupNavitionBar()
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    fileprivate func setupNavitionBar(){
+        
+        //1.设置左按钮
+//        navigationItem.leftBarButtonItem = UIBarButtonItem.createItem(imageName: "logo")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(imageName: "logo")
+        
+        //2.设置右按钮
+        let  size = CGSize(width: 40, height: 40)
+        let historyBtn = UIBarButtonItem(imageName: "image_my_history", highImage: "Image_my_history_click", size: size)
+        let searchBtn = UIBarButtonItem(imageName: "btn_search", highImage: "btn_search_clicked", size: size)
+        let qrcodeItem = UIBarButtonItem(imageName: "Image_scan", highImage: "Image_scan_click", size: size)
+        //按钮创建布局由右往左
+        navigationItem.rightBarButtonItems = [historyBtn,searchBtn,qrcodeItem]
     }
-    */
-
+    
 }
--- DouYuTV/DouYuTV/Classes/Tools/Extention/UIBarButtonItem+Extention.swift
+++ DouYuTV/DouYuTV/Classes/Tools/Extention/UIBarButtonItem+Extention.swift
@@ -6,4 +6,50 @@
 //  Copyright © 2017年 guokai. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIBarButtonItem{
+    
+    /*
+     //创建一个类方法
+     class func  createItem(imageName : String ,highImage : String = "" , size : CGSize = CGSize.zero) -> UIBarButtonItem {
+     let btn = UIButton()
+     
+     btn.setImage(UIImage(named : imageName), for: .normal)
+     btn.setImage(UIImage(named : highImage), for: .highlighted)
+     if size == CGSize.zero {
+     
+     btn.sizeToFit()
+     
+     }else{
+     
+     btn.frame = CGRect(origin: CGPoint.zero, size: size)
+     }
+     return UIBarButtonItem(customView: btn)
+     }
+     */
+   
+    //便利构造函数 1> convenience开头,2>在构造函数中必须明确调用一个设计构造函数(必须使用self)
+    //可以设置默认值。
+    convenience init(imageName : String ,highImage : String = "" , size : CGSize = CGSize.zero){
+        //1.创建UIButton
+        let btn = UIButton()
+        
+        //2.设置btn的图片
+        btn.setImage(UIImage(named : imageName), for: .normal)
+        if highImage != "" {
+            
+            btn.setImage(UIImage(named : highImage), for: .highlighted)
+        }
+        //3.设置btn的尺寸
+        if size == CGSize.zero {
+            
+            btn.sizeToFit()
+        }else{
+            btn.frame = CGRect(origin: CGPoint.zero, size: size)
+        }
+      
+        //4.创建UIBarButtonItem
+        self.init(customView: btn)
+    }
+}
